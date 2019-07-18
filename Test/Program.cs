ecd0ea07c33f86a22fe225efcd5c2eacfc3b4932
lumen13 <ntap13@gmail.com>
2019-07-18 16:57:37 +0300

    "Students (Test)" and "OOP #1 Nasledovanie (Test1)" created! Need to continue to watch lessons

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,27 +2,61 @@
 
 namespace Test
 {
-    class Program
+    public class Human
     {
+        protected string creature;
+        protected string institute;
+
+        protected Human(string _creature, string _institute)
+        {
+            creature = _creature;
+            institute = _institute;
+        }
+    }
+    class Student : Human
+    {
+        protected string name; 
+        protected string faculty;
+        protected int group;
+        protected string gender;
+        protected int age;
+        protected bool grant;  
+        
+        protected Student(string _name, string _creature, string _institute, string _faculty,
+            int _group, string _gender, int _age, bool _grant) : base (_creature, _institute)
+        {
+            name = _name;
+            faculty = _faculty;
+            group = _group;
+            gender = _gender;
+            age = _age;
+            grant = _grant;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("Имя: " + name + " ");
+            Console.WriteLine("Статус: " + creature + " ");
+            Console.WriteLine("Универ: " + institute + " ");
+            Console.WriteLine("Факультет: " + faculty + " ");
+            Console.WriteLine("Группа: " + group + " ");
+            Console.WriteLine("Гендер: " + gender + " ");
+            Console.WriteLine("Возраст: " + age + " ");
+            Console.WriteLine("Степуха: " + grant + "\n");
+        }
+
         static void Main(string[] args)
         {
-            // Объявляем двумерный массив
-            int[,] myArr = new int[4, 5];
-
-            Random ran = new Random();
-
-            // Инициализируем данный массив
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    myArr[i, j] = ran.Next(1, 15);
-                    Console.Write("{0}\t", myArr[i, j]);
-                }
-                Console.WriteLine();
-            }
+            Student Andrei = new Student("Андрей", "Студент", "СевГУ", "КиВТ", 740, "Мужик", 99, true);
+            Student Alexey = new Student("Алексей", "Студент", "СевГУ", "ИиРТ", 740, "Парень", 22, true);
+            Alexey.Output();
+            Andrei.Output();
 
             Console.ReadKey();
         }
-    }
+    }    
 }
+
+
+
+
---
+++ Test1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Test1
+{
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+    class Dog : Animal
+    {
+        protected float speed;
+        public Dog (float speed, string name) : base (name)
+        {
+            this.speed = speed;
+            Console.WriteLine("Speed: " + speed);
+        }        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog jack = new Dog(12.23f, "Jack")
+            {
+                Name = "Jack"
+            };
+            Console.WriteLine(jack.Name + " ");
+            jack.Print();
+
+            Dog bob = new Dog(5.5f, "Bob")
+            {
+                Name = "Bob"
+            };
+            Console.WriteLine(bob.Name + " ");
+            bob.Print();
+
+            Console.ReadKey();         
+        }
+    }
+}
