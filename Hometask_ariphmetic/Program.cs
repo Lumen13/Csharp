c62d0fb6b171a60d8f9b48e805836b93834c56f4
Lumen13 <47292737+Lumen13@users.noreply.github.com>
2019-12-14 18:17:35 +0300

    Up. I need to start Test.sln only

--- Hometask_ariphmetic/Program.cs
+++ Hometask_ariphmetic/Program.cs
@@ -6,7 +6,20 @@ namespace Hometask_ariphmetic
     {
         static void Main(string[] args)
         {
+            double FirstValue, SecondValue, ThirdValue;
 
+            Console.WriteLine("Программа по вычеслению суммы и произведения трёх чисел" +
+                "\nВведите первое число");
+            FirstValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            SecondValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число");
+            ThirdValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Сумма трёх чисел = {FirstValue + SecondValue + ThirdValue}");
+            Console.WriteLine($"Произведение трёх чисел = {FirstValue * SecondValue * ThirdValue}");
         }
     }
 }
---
+++ Test/hometask_divisible-by-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hometask_divisible_by_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double NumInput;
+
+            Console.WriteLine("Программа проверки чётности числа" +
+                "\n\nВведите число");
+            NumInput = Convert.ToDouble(Console.ReadLine());
+
+            if (NumInput % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+        }
+    }
+}
