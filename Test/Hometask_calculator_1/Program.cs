2ba1feae28264dfd4171b29261504b5e48544f88
Lumen13 <47292737+Lumen13@users.noreply.github.com>
2019-12-19 08:42:50 +0300

    Up

---
+++ Test/Hometask_calculator_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hometask_calculator_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число");
+            double FirstValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите символ математической операции (+ - * /)");
+            string Operator = Console.ReadLine();
+            Console.WriteLine("Введите 2 число");
+            double SecondValue = double.Parse(Console.ReadLine());
+
+            if (Operator == "+")
+            {
+                Console.WriteLine(FirstValue + SecondValue);
+            }
+            else if (Operator == "-")
+            {
+                Console.WriteLine(FirstValue - SecondValue);
+            }
+            else if (Operator == "*")
+            {
+                Console.WriteLine(FirstValue * SecondValue);
+            }
+            else if (Operator == "/")
+            {
+                Console.WriteLine(FirstValue / SecondValue);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Неизвестный оператор");
+            }
+        }
+    }
+}
---
+++ Test/Hometask_calculator_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hometask_calculator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число");
+            double FirstValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите символ математической операции (+ - * /)");
+            string Operator = Console.ReadLine();
+            Console.WriteLine("Введите 2 число");
+            double SecondValue = double.Parse(Console.ReadLine());
+
+            switch (Operator)
+            {
+                case "+":
+                    Console.WriteLine(FirstValue+SecondValue);
+                    break;
+                case "-":
+                    Console.WriteLine(FirstValue-SecondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(FirstValue*SecondValue);
+                    break;
+                case "/":
+                    Console.WriteLine(FirstValue/SecondValue);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка ввода!");
+                    break;
+            }
+        }
+    }
+}
