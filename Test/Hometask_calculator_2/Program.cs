3d4cdf3abb1781b7aaa2c77108af8928ba44c7f5
Lumen13 <47292737+Lumen13@users.noreply.github.com>
2019-12-21 15:48:59 +0300

    Up

--- Test/Hometask_calculator_2/Program.cs
+++ Test/Hometask_calculator_2/Program.cs
@@ -5,31 +5,66 @@ namespace Hometask_calculator_2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите 1 число");
-            double FirstValue = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите символ математической операции (+ - * /)");
-            string Operator = Console.ReadLine();
-            Console.WriteLine("Введите 2 число");
-            double SecondValue = double.Parse(Console.ReadLine());
+        {            
+            double FirstValue, SecondValue;
+            string Operator;
 
-            switch (Operator)
+            while (true)
             {
-                case "+":
-                    Console.WriteLine(FirstValue+SecondValue);
-                    break;
-                case "-":
-                    Console.WriteLine(FirstValue-SecondValue);
-                    break;
-                case "*":
-                    Console.WriteLine(FirstValue*SecondValue);
-                    break;
-                case "/":
-                    Console.WriteLine(FirstValue/SecondValue);
-                    break;
-                default:
-                    Console.WriteLine("Ошибка ввода!");
-                    break;
+                Console.Clear();
+
+                try
+                {
+                    Console.WriteLine("Введите 1 число");
+                    FirstValue = double.Parse(Console.ReadLine());                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка! Необходимо ввести число");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Введите символ математической операции (+ - * /)");
+                Operator = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine("Введите 2 число");
+                    SecondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка! Необходимо ввести число");
+                    continue;
+                }
+
+                switch (Operator)
+                {
+                    case "+":
+                        Console.WriteLine(FirstValue + SecondValue);
+                        break;
+                    case "-":
+                        Console.WriteLine(FirstValue - SecondValue);
+                        break;
+                    case "*":
+                        Console.WriteLine(FirstValue * SecondValue);
+                        break;
+                    case "/":
+                        if (SecondValue == 0)
+                        {
+                            Console.WriteLine("Ошибка деления на 0!");
+                            break;
+                        }
+                        Console.WriteLine(FirstValue / SecondValue);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка ввода операции!");
+                        break;
+                }
+
+                Console.ReadLine();
+                Console.Clear();
             }
         }
     }
---
+++ Test/Hometask_parity_counter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hometask_parity_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                int FirstValue, SecondValue, CheckParity, CheckRecursion;                
+                Console.WriteLine("Введите 2 целых числа, чтобы задать диапазон проверки четности\n");
+
+                try
+                {
+                    Console.WriteLine("Проверяем от числа...");
+                    FirstValue = int.Parse(Console.ReadLine());
+                    Console.WriteLine("и до числа...");
+                    SecondValue = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nВведённое значение не является целым числом или оно слишком длинное");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                if (FirstValue >= SecondValue)
+                    Console.WriteLine("Неверный диапазон! Первое число должно быть меньше второго");
+
+                CheckRecursion = SecondValue - FirstValue;
+                if (CheckRecursion >= 1001)
+                {
+                    Console.WriteLine("Вы ввели слишком большой диапазон чисел!\n" +
+                        "Попробуйте ввести 2 приближенных друг к другу по значениям числа");
+                    Console.ReadLine();
+                    continue;
+                }                    
+
+                while (FirstValue <= SecondValue)
+                {
+                    CheckParity = FirstValue % 2;
+                    if (CheckParity == 0)
+                        Console.WriteLine($"{FirstValue} - четное");
+                    else
+                        Console.WriteLine($"{FirstValue} - не четное");
+                    FirstValue++;
+                }
+
+                Console.ReadLine();
+            }            
+        }
+    }
+}
