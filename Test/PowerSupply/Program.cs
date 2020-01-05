53adba6cd6c0d8dc48bc82ac6728ee0a3ab949c8
lumen13 <ntap13@gmail.com>
2020-01-05 23:38:09 +0300

    Up

---
+++ Test/PowerSupply/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PowerSupply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*      Программа расчёта мощности БП
+             *      для ПК в зависимости от комплектующих.
+             *      Методы со ссылками и всевозможными параметрами
+             */
+
+            int Result = Calculation
+                (x: int.Parse(Console.ReadLine()),
+                 y: int.Parse(Console.ReadLine())
+                 );
+
+            Console.WriteLine(Result);
+        }
+
+        static int Calculation(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    }
+}
